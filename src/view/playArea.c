5442b6925653ff9e12d783e4971dad6e90ae1ca5
LoickBde <loick.baude@ig2i.centralelille.fr>
2020-03-29 12:44:51 +0200

    add quit  event

--- src/view/playArea.c
+++ src/view/playArea.c
@@ -5,6 +5,7 @@ int main(int argc, char *argv[])
     SDL_Window *window = NULL; //Pointeur sur la fenêtre
     SDL_Renderer *renderer = NULL; //Pointeur sur le renderer
     SDL_Texture *texture_foreground, *texture_background = NULL; 
+    SDL_bool prgm_running = SDL_TRUE;  
 
     initSDLbasics(&window, &renderer, "Zone de jeu"); //Initialise les bases de la sdl (fenetre, renderer)
 
@@ -17,10 +18,24 @@ int main(int argc, char *argv[])
     
     setGameBoard(&renderer, texture_foreground, texture_background); //Mise en place de la zone de jeu
     SDL_RenderPresent(renderer); //Maj de l'affichage 
- 
-    SDL_Delay(5000); //Pause 
-
-    SDL_DestroyTexture(texture_foreground); 
+    
+    while(prgm_running)
+    {
+        SDL_Event event; 
+        while(SDL_PollEvent(&event))
+        {
+            switch(event.type)
+            {
+                case SDL_QUIT : 
+                    prgm_running = SDL_FALSE; 
+                    break; 
+                default :
+                    break; 
+            }
+        }
+    }
+    
+    SDL_DestroyTexture(texture_foreground); //Détruit les textures
     SDL_DestroyTexture(texture_background); 
     SDL_DestroyRenderer(renderer); //Detruit le render
     SDL_DestroyWindow(window); //Détruit la fenetre
